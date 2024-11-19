37eceac5cdf17a3107a848ef89cc1ef784bcf46e
cleru <cleru@192.168.0.109>
2024-11-19 19:49:51 -0400

    Pasos de prueba unitaria

---
+++ Object Repository/Page_BOSCOLISTA/div_Personal        1 Tasks.rs
@@ -0,0 +1,113 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Personal        1 Tasks</name>
+   <tag></tag>
+   <elementGuidId>4a22c087-7798-48b1-b175-c138368ce7cf</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.category</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='img'])[1]/following::div[3]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>.category >> nth=0</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>b564e2b7-0f9a-4ff7-bbff-f65bd7838b27</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>category</value>
+      <webElementGuid>d84a60cf-355a-4e4f-8419-a606ca8f03dd</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+      
+        Personal
+        1 Tasks
+      
+    </value>
+      <webElementGuid>76606d5b-2f9f-40a8-988c-da5ed7771a7e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/div[@class=&quot;wrapper&quot;]/div[@class=&quot;home-screen screen&quot;]/div[@class=&quot;categories-wrapper&quot;]/div[@class=&quot;categories&quot;]/div[@class=&quot;category&quot;]</value>
+      <webElementGuid>fa8fbd76-cd48-42eb-9494-116471e61ab7</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='img'])[1]/following::div[3]</value>
+      <webElementGuid>eda38859-8fbe-4e3a-a6cf-2d0c7ef65ffc</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Hola, bienvenido/a'])[1]/following::div[5]</value>
+      <webElementGuid>197cb7f3-525d-4c1d-8272-3c1cf18f97d4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Trabajo'])[1]/preceding::div[2]</value>
+      <webElementGuid>600fe850-82bf-479f-aa22-46024bfe628d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div[2]/div/div</value>
+      <webElementGuid>93995cd6-de17-4dfd-88fb-1e2f1489a6c6</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+      
+        Personal
+        1 Tasks
+      
+    ' or . = '
+      
+        Personal
+        1 Tasks
+      
+    ')]</value>
+      <webElementGuid>93a8d08b-5a26-42ea-aee2-383d2b0a56f2</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Page_BOSCOLISTA/p_1 Tasks.rs
@@ -0,0 +1,98 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>p_1 Tasks</name>
+   <tag></tag>
+   <elementGuidId>2285b341-33af-4094-bde7-44e7ac8e48fc</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Trabajo'])[1]/following::p[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>.categories > div:nth-child(2) > .content > p</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>p</value>
+      <webElementGuid>d390545e-79da-4c2d-a3ef-ed2b45d43dc4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>1 Tasks</value>
+      <webElementGuid>e4530e9b-989a-44d4-8236-0b141ab30d6e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/div[@class=&quot;wrapper&quot;]/div[@class=&quot;home-screen screen&quot;]/div[@class=&quot;categories-wrapper&quot;]/div[@class=&quot;categories&quot;]/div[@class=&quot;category&quot;]/div[@class=&quot;content&quot;]/p[1]</value>
+      <webElementGuid>b2e92870-0097-46ce-ac8e-8a2f0b805e19</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Trabajo'])[1]/following::p[1]</value>
+      <webElementGuid>0c5329ff-5538-4a2c-b69a-d7b295848556</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Personal'])[1]/following::p[2]</value>
+      <webElementGuid>d6df5a60-080e-46bc-b810-88ba1b8bc62b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Compras'])[1]/preceding::p[1]</value>
+      <webElementGuid>ad550fa6-7472-475c-8df8-7ad638785770</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Citas'])[1]/preceding::p[2]</value>
+      <webElementGuid>1425b7f6-1d99-4bb5-942d-e0494c59a1f0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div[2]/div/div[2]/div/p</value>
+      <webElementGuid>6a40aaa5-7447-4739-bd17-a0c70ec91cc8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//p[(text() = '1 Tasks' or . = '1 Tasks')]</value>
+      <webElementGuid>bb9044dc-6987-4769-baac-4835a54982f5</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
